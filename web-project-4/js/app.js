f88c74b12586e9aa9b4fe83b259231d6d9552649
Roman <32957243+romanlevonovyy@users.noreply.github.com>
2020-12-09 01:19:21 +0200

    web-project-4

---
+++ web-project-4/js/app.js
@@ -0,0 +1,76 @@
+$(function() {
+	
+	var header = $("#header"), 
+	  introH = $("#intro").innerHeight(), 
+	  scrollOffSet = $(window).scrollTop();
+	
+	
+	/* Fixed Header */
+	checkScroll(scrollOffSet);
+	
+	$(window).on("scroll", function() {
+		scrollOffSet = $(this).scrollTop();
+		
+		checkScroll(scrollOffSet);
+	});
+	
+	
+	function checkScroll(scrollOffSet) {
+		if (scrollOffSet >= introH) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+	  }
+	}
+	
+	
+	/* Smoth scroll */
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+		
+		var $this = $(this),
+		    blockId = $this.data("scroll"),
+			blockOffSet = $(blockId).offset().top;
+		
+		$("#nav a").removeClass("active")
+		$this.addClass("active");
+		
+		$("html , body").animate({
+			scrollTop: blockOffSet
+		}, 500);
+	});
+	
+	
+	/* Menu nav toggle */
+	$("#nav_toggle").on("click", function(event) {
+		event.preventDefault();
+		
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	})
+	
+	
+	
+	/* Collapse */
+	$("[data-collapse]").on("click", function(event) {
+		event.preventDefault();
+		
+		var $this = $(this),
+		    blockId = $this.data("collapse");
+		
+		$(this).toggleClass("active");
+		$(blockId).slideToggle();
+	});
+	
+	
+	
+	
+	
+	
+	
+});
+
+
+
+
+
